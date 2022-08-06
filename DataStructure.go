7a1faf14dd0460eabe92ad0ef9417e41b61d14c1
Jaehyun Kim <70624258+a7420174@users.noreply.github.com>
2022-08-06 17:36:26 +0900

    Create DataStructure.go

---
+++ DataStructure.go
@@ -0,0 +1,45 @@
+package main
+
+import "container/list"
+
+type Queue struct {
+	v *list.List
+}
+
+func NewQueue() *Queue {
+	return &Queue{list.New()}
+}
+
+func (q *Queue) Push(v interface{}) {
+	q.v.PushBack(v)
+}
+
+func (q *Queue) Pop() interface{} {
+	front := q.v.Front()
+	if front == nil {
+		return nil
+	}
+
+	return q.v.Remove(front)
+}
+
+type Stack struct {
+	v *list.List
+}
+
+func NewStack() *Stack {
+	return &Stack{list.New()}
+}
+
+func (q *Stack) Push(v interface{}) {
+	q.v.PushBack(v)
+}
+
+func (q *Stack) Pop() interface{} {
+	back := q.v.Back()
+	if back == nil {
+		return nil
+	}
+
+	return q.v.Remove(back)
+}
