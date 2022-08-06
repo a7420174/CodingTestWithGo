fefa2b2f769a4694f80aa7523500b7f2c704b9eb
Jaehyun Kim <70624258+a7420174@users.noreply.github.com>
2022-08-06 21:13:43 +0900

    Update DFS.go

--- DFS.go
+++ DFS.go
@@ -8,7 +8,7 @@ func main() {
 	/*
 	출처: 이것이 취업을 위한 코딩 테스트다 with 파이썬 (나동빈 저)
 	
-	DFS(깊이 우선 탐색) 예제
+	BFS(너비 우선 탐색) 예제
 	*/
 	graph := [9][]int{
 		{},
@@ -24,17 +24,18 @@ func main() {
 
 	var visited [9]bool
 
-	var DFS func(int)
-	DFS = func(v int){
-		visited[v] = true
-		fmt.Printf("%v ", v)
-		for _, i := range graph[v] {
-		if !visited[i] {
-			DFS(i)
-		}
-		}
-	}
-	DFS(1)
+	// var DFS func(int)
+	// DFS = func(v int){
+	// 	visited[v] = true
+	// 	fmt.Printf("%v ", v)
+	// 	for _, i := range graph[v] {
+	// 		if !visited[i] {
+	// 			DFS(i)
+	// 		}
+	// 	}
+	// }
+	// DFS(1)
+	
 	DFS(&graph, 1, &visited)
 	fmt.Println()
 }
